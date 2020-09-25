3dd126794fa6b165fdf3207a520346693edb1ea6
Anton Miroshnyk <miroshniktyt@gmail.com>
2020-09-25 22:43:51 +0300

    Initial Commit

---
+++ dummy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  dummy
+//
+//  Created by Macbook Air on 25.09.2020.
+//  Copyright © 2020 macbook. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
